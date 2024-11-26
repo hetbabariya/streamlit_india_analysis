9c81e733abed48b2ce090667e34ef9787116845f
hetbabariya <hetbabriyabali09@gmail.com>
2024-11-26 14:33:00 +0530

    change file path

--- utility.py
+++ utility.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-df = pd.read_csv(r"C:\Users\hetba\OneDrive\Desktop\Work\Datascience\pythonPro\india_Analysis\merge_india_census.csv")
+df = pd.read_csv(r".\merge_india_census.csv")
 
 
 
